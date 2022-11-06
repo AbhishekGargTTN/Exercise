82d22c2d8c19dccc4b9d5a102ee146088654f3f8
AbhishekGargTTN <114075294+AbhishekGargTTN@users.noreply.github.com>
2022-11-06 16:21:00 +0530

    Added Rest Services source code files for question 3,4,5,7

---
+++ RESTful-Web-Services/src/test/java/com/GargAbhishek/RESTful/Web/Services/ResTfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GargAbhishek.RESTful.Web.Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
